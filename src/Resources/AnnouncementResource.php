6d8a4ccfee69e297d7282e080ccc8e98eba2ef90
Elsevar <rizaeve@bk.ru>
2024-02-19 10:49:39 +0000

    Update AnnouncementResource.php
    Used Toggle instead of Checkbox, and also moved elements into a Section for better visibility.

--- src/Resources/AnnouncementResource.php
+++ src/Resources/AnnouncementResource.php
@@ -4,9 +4,12 @@ namespace Rupadana\FilamentAnnounce\Resources;
 
 use App\Models\User;
 use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Resources\Resource;
@@ -14,7 +17,6 @@ use Filament\Support\Facades\FilamentColor;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Forms\Components\Checkbox;
 use Guava\FilamentIconPicker\Forms\IconPicker;
 use Guava\FilamentIconPicker\Tables\IconColumn;
 use Rupadana\FilamentAnnounce\Models\Announcement;
@@ -30,33 +32,38 @@ class AnnouncementResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')
-                    ->minLength(5)
-                    ->required(),
-                TextInput::make('title')
-                    ->minLength(5)
-                    ->required(),
-                Textarea::make('body')
-                    ->minLength(20)
-                    ->required(),
-                IconPicker::make('icon'),
-                Select::make('color')
-                    ->options([
-                        ...collect(FilamentColor::getColors())->map(fn ($value, $key) => ucfirst($key))->toArray(),
-                        'custom' => 'Custom',
-                    ])
-                    ->live(),
-                ColorPicker::make('custom_color')
-                    ->hidden(fn (Get $get) => $get('color') != 'custom')
-                    ->requiredIf('color', 'custom')
-                    ->rgb(),
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->minLength(5)
+                            ->required(),
+                        TextInput::make('title')
+                            ->minLength(5)
+                            ->required(),
+                        Textarea::make('body')
+                            ->minLength(20)
+                            ->required(),
+                        IconPicker::make('icon'),
+                        Select::make('color')
+                            ->options([
+                                ...collect(FilamentColor::getColors())->map(fn($value, $key) => ucfirst($key))->toArray(),
+                                'custom' => 'Custom',
+                            ])
+                            ->live(),
+                        ColorPicker::make('custom_color')
+                            ->hidden(fn(Get $get) => $get('color') != 'custom')
+                            ->requiredIf('color', 'custom')
+                            ->rgb(),
 
-                 Select::make('users')
-                    ->options(User::pluck('name', 'id')->toArray())
-                    ->multiple()
-                    ->required(),
+                        Select::make('users')
+                            ->options(User::pluck('name', 'id')->toArray())
+                            ->hidden(fn(Get $get) => $get('all') == true)
+                            ->multiple()
+                            ->required(),
+                    ])->columns(2),
 
-                Checkbox::make('all')
+                Toggle::make('all')
+                    ->live()
                     ->label('All users')
                     ->default(false),
             ]);
