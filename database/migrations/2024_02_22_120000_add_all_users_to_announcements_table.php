d5d0f73d285ea06d0f55bc0597735b852b7ba345
Elsevar <rizaeve@bk.ru>
2024-02-22 08:09:07 +0000

    Create 2024_02_22_120000_add_all_users_to_announcements_table.php

---
+++ database/migrations/2024_02_22_120000_add_all_users_to_announcements_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            $table->boolean('all_users')
+                ->default(false)
+                ->after('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('announcements', function (Blueprint $table) {
+            //
+        });
+    }
+};
